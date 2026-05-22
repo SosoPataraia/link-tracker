b74fa3f5ea4692d6a2c14bdea415b0daa9a84510
User <ipata2019@agruni.edu.ge>
2026-05-22 15:48:56 +0300

    refactor: apply structured logging, @Data DTOs, TelegramBotAdapter, Dead code cleanup, unused config

--- bot/src/main/java/backend/academy/linktracker/bot/dto/RemoveLinkRequest.java
+++ bot/src/main/java/backend/academy/linktracker/bot/dto/RemoveLinkRequest.java
@@ -1,12 +1,10 @@
 package backend.academy.linktracker.bot.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class RemoveLinkRequest {
